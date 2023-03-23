288847bbce11a11365a3d25c3838b8c64b3852cb
Iqra <iqra.ali@student.moringaschool.com>
2023-03-23 23:28:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
-
-// Code your solution here
+var totalBatteries = 31;
+totalBatteries.reduce(function(showcases, battery) { showcases[batterySorter.showcaseAssign(battery)].push(battery)},allshowcases)
